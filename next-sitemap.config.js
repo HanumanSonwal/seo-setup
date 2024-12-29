8db50bac6fc8ec065ae55b588a2c79d1a010b230
aditya <asonwal5@gmail.com>
2024-12-29 08:38:30 +0530

    new canges

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://yourwebsite.com', // Replace with your domain
-    generateRobotsTxt: true, // Create robots.txt
+    siteUrl: 'https://seo-setup.vercel.app', // Replace with your actual domain
+    generateRobotsTxt: true, // (Optional) Generate robots.txt
+    sitemapSize: 5000, // Adjust the limit of URLs per sitemap (if needed)
+    generateIndexSitemap: true, // Create an index sitemap
   };
   
